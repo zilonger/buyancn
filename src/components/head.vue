18cba853c4da9f6aa5e0b76ebefc348dbcd3a41b
zilonger <410992025@qq.com>
2018-04-23 22:28:13 +0800

    import bootstrap

---
+++ src/components/head.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="col-9">
+    <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Head',
+  data () {
+    return {
+      msg: 'header'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Head from '@/components/Head'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 Vue.use(Router)
 
@@ -8,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Head',
+      component: Head
     }
   ]
 })

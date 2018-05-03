96741e96f0943c4d73cb5af1e7a1ab33162d8999
zilonger <410992025@qq.com>
2018-05-03 23:40:03 +0800

    添加两张banner

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -9,25 +9,9 @@
           :radius-dot="setting.radiusDot"
           :trigger="setting.trigger"
           :arrow="setting.arrow">
-          <CarouselItem>
+          <CarouselItem v-for="(item,index) in banners" :key="index">
               <div class="demo-carousel swiper-item">
-                <!-- <a href="#"><img src="http://via.placeholder.com/1920x820" alt=""></a> -->
-                <a href="#"><img src="../assets/images/banner_1.jpg" alt=""></a>
-              </div>
-          </CarouselItem>
-          <CarouselItem>
-              <div class="demo-carousel swiper-item">
-                <a href="#"><img src="http://via.placeholder.com/1920x820" alt=""></a>
-              </div>
-          </CarouselItem>
-          <CarouselItem>
-              <div class="demo-carousel swiper-item">
-                <a href="#"><img src="http://via.placeholder.com/1920x820" alt=""></a>
-              </div>
-          </CarouselItem>
-          <CarouselItem>
-              <div class="demo-carousel swiper-item">
-                <a href="#"><img src="http://via.placeholder.com/1920x820" alt=""></a>
+                <a :href="item.link"><img :src="item.url" alt=""></a>
               </div>
           </CarouselItem>
       </Carousel>
@@ -58,72 +42,86 @@
 
 <script>
 export default {
-  name: 'Home',
-  data () {
+  name: "Home",
+  data() {
     return {
-        value3: 0,
-        setting: {
-            autoplay: false,
-            autoplaySpeed: 3000,
-            dots: 'inside',
-            radiusDot: false,
-            trigger: 'click',
-            arrow: 'hover'
+      value3: 0,
+      setting: {
+        autoplay: false,
+        autoplaySpeed: 3000,
+        dots: "inside",
+        radiusDot: false,
+        trigger: "click",
+        arrow: "hover"
+      },
+      banners: [
+        {
+          link: "#",
+          url: "assets/images/banner_1.jpg"
+        },
+        {
+          link: "#",
+          url: "assets/images/banner_2.jpg"
+        },
+        {
+          link: "#",
+          url: "assets/images/banner_3.jpg"
         }
-    }
+      ]
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .swiper-item img {
-    width: 100%;
-  }
-  .blockwrap{
-    margin: 0 auto;
-    max-width: 1170px;
-    padding-top: 45px;
-    padding-bottom: 45px;
-  }
-  .infoblock{
-    background-color: #ffffff;
-    padding: 15px;
-    border: 1px solid #e9eaec;
-  }
-  .infoblock:hover{
-    box-shadow: 0 1px 6px rgba(0,0,0,.2);
-    border-color: #eee;
-  }
-  .infoblock:hover > img{
-    opacity: 1;
-  }
-  .infoblock img{
-    width: 100%;
-    opacity: .6;
-    transition: all 0.3s linear;
-  }
-  .infoblock p{
-    color: #3f424b;
-    padding: 10px 5px;
-    line-height: 16px;
-    height: 68px;
-  }
-  .blocktit{
-    color: #3f424b;
-    font-weight: normal;
-    line-height: 30px;
-    margin-bottom: 10px;
-  }
-  .blocktit a{
-    color: #858b9a;
-    float: right;
-  }
-  .addrpic{
-    text-align: center;
-    background-color: #455090;
-  }
-  .addrpic img {
-    max-width: 100%;
-  }
+.swiper-item img {
+  width: 100%;
+}
+.blockwrap {
+  margin: 0 auto;
+  max-width: 1170px;
+  padding-top: 45px;
+  padding-bottom: 45px;
+}
+.infoblock {
+  background-color: #ffffff;
+  padding: 15px;
+  border: 1px solid #e9eaec;
+}
+.infoblock:hover {
+  box-shadow: 0 1px 6px rgba(0, 0, 0, 0.2);
+  border-color: #eee;
+}
+.infoblock:hover > img {
+  opacity: 1;
+}
+.infoblock img {
+  width: 100%;
+  opacity: 0.6;
+  transition: all 0.3s linear;
+}
+.infoblock p {
+  color: #3f424b;
+  padding: 10px 5px;
+  line-height: 16px;
+  height: 68px;
+}
+.blocktit {
+  color: #3f424b;
+  font-weight: normal;
+  line-height: 30px;
+  margin-bottom: 10px;
+}
+.blocktit a {
+  color: #858b9a;
+  float: right;
+}
+.addrpic {
+  text-align: center;
+  background-color: #455090;
+}
+.addrpic img {
+  max-width: 100%;
+}
 </style>
